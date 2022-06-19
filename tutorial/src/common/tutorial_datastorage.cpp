0761b7066be2ba932107cdc43b52264811d8d18d
pbert <p.engelbert@posteo.net>
2022-06-19 13:46:41 +0200

    move tutorial from old revision to head

---
+++ tutorial/src/common/tutorial_datastorage.cpp
@@ -0,0 +1,54 @@
+/** \example tutorial_identdatastorage.cpp
+ * Example implementation for storing identdata with the eeprom tutorial.
+ */
+
+#include <IsoAgLib/isoaglib_config.h>
+#include "tutorial_identdatastorage.h"
+
+namespace IsoAgLibTutorial {
+
+  tutorialIdentDataStorage_c::tutorialIdentDataStorage_c(const uint16_t a_baseAddr) {
+#ifdef USE_EEPROM_IO
+    mc_eeprom.init(a_baseAddr, m_eepromBlockSize, m_eepromMagicPattern);
+#endif
+  }
+
+  uint8_t tutorialIdentDataStorage_c::_loadSa() {
+#ifdef USE_EEPROM_IO
+    uint8_t sa = m_defaultSa;
+    if (mc_eeprom.valid() && mc_eeprom.read(m_eepromSaOffset, sa)) {
+      return sa;
+    }
+#endif
+    return m_defaultSa;
+  }
+
+  void tutorialIdentDataStorage_c::_storeSa(const uint8_t a_sa) {
+#ifdef USE_EEPROM_IO
+    mc_eeprom.write(m_eepromSaOffset, a_sa);
+    mc_eeprom.setValid();
+#endif
+  }
+
+  void tutorialIdentDataStorage_c::_loadDtcs( IsoAgLib::iDtcContainer_c& /*arc_dtcContainer*/ )
+  {
+    // @TODO load DTCs
+  }
+
+  void tutorialIdentDataStorage_c::_storeDtcs( const IsoAgLib::iDtcContainer_c& /*arc_dtcContainer*/ )
+  {
+    // @TODO save DTCs
+  }
+
+  void tutorialDataStorage_c::_loadPreferredVt( IsoAgLib::iIsoName_c& /*arc_isoname*/, uint8_t& /*arui8_boottime_s*/ )
+  {
+    // @TODO load Preferred VT infos
+  }
+
+  void tutorialDataStorage_c::_storePreferredVt( const IsoAgLib::iIsoName_c& /*arc_isoname*/, uint8_t /*aui8_bootTime*/ )
+  {
+    // @TODO save Preferred VT infos
+  }
+}
+
+// eof

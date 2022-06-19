4501999cabc4c83fe503935e3fccd35b227040bc
pbert <p.engelbert@posteo.net>
2022-06-19 13:52:19 +0200

    regnerate objectpool with new vt2iso version

--- tutorial/src/ecu_display/component_vtclient_pool/tutorialDisplay_derived-cpp.h
+++ tutorial/src/ecu_display/component_vtclient_pool/tutorialDisplay_derived-cpp.h
@@ -1,8 +1,19 @@
-#include "tutorialDisplay-defines.inc"
 #include "tutorialDisplay-variables.inc"
+
+#if defined( USE_SECTION_VT_OBJECT_POOL )
+// Begin section vt_object_pool
+#  pragma section . vt_object_pool
+#endif
+
+#include "tutorialDisplay-defines.inc"
 #include "tutorialDisplay-attributes.inc"
 #include "tutorialDisplay-list00.inc"
 #include "tutorialDisplay-list01.inc"
 #include "tutorialDisplay-list.inc"
 #include "tutorialDisplay-list_attributes.inc"
 #include "tutorialDisplay-functions.inc"
+
+#if defined( USE_SECTION_VT_OBJECT_POOL )
+// End section vt_object_pool
+#  pragma section
+#endif
